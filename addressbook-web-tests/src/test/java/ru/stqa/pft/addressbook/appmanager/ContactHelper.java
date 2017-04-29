53d6ce979e8e4040fc11cc373356ba7776ce9478
Denys Zverev <dzdenya@gmail.com>
2017-04-29 14:21:38 +0300

    Разница xpass и css

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -44,7 +44,7 @@ public class ContactHelper extends HelperBase {
   }
 
   public void initContactModification() {
-    click(By.xpath("//img[@alt = 'Edit']"));
+    click(By.cssSelector("img[alt = 'Edit']")); //тоже самое> click(By.xpath("//img[@alt = 'Edit']"));
   }
 
   public void submitContactModification() {
