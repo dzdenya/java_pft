020562316c7ccc1b474824b1c6f2f69ce798b7d2
Denys Zverev <dzdenya@gmail.com>
2017-04-30 11:54:38 +0300

    Коллекции: наборы элементов

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,26 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Denys on 4/30/2017.
+ */
+public class Collections {
+  
+  public static void main (String[] args){
+    String[] langs = {"java","C#","Python","php"};
+
+    List<String> languages = Arrays.asList("java","C#","Python","php");
+//    languages.add("Java");
+//    languages.add("C#");
+//    languages.add("Pyhton");
+//    languages.add("PHP");
+
+    for (String lang : languages) {
+      System.out.println("Я хочу выучить " + lang);
+    }
+
+  }
+}
