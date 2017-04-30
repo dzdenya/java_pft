81a0e392692bbf5230a52fa290450b3944922b6a
Denys Zverev <dzdenya@gmail.com>
2017-04-30 15:46:42 +0300

    Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,10 +6,8 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-
-
   public GroupData(String name, String header, String footer) {
-    this.id = 0;
+    this.id = Integer.MAX_VALUE;
     this.name = name;
     this.header = header;
     this.footer = footer;
@@ -50,6 +48,7 @@ public class GroupData {
     return footer;
   }
 
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -57,14 +56,12 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+    return name != null ? name.hashCode() : 0;
   }
+
 }
